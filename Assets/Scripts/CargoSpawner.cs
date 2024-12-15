c4fc745b1d7fba151d4454499362822379b7488b
SezerCan17 <cans76600@gmail.com>
2024-12-15 18:40:20 +0300

    VFX

--- Assets/Scripts/CargoSpawner.cs
+++ Assets/Scripts/CargoSpawner.cs
@@ -30,21 +30,21 @@ public class CargoSpawner : MonoBehaviour
             GameObject spawnedCargo = Instantiate(selectedCargo.cargoPrefab, spawnPoint[i].position, Quaternion.identity);
             spawnedCargo.name = selectedCargo.cargoName;
 
-            // Package script'ini al ve CargoSO'yu ata
+            
             Package packageScript = spawnedCargo.GetComponent<Package>();
             if (packageScript != null)
             {
                 packageScript.cargoSO = selectedCargo;
             }
 
-            // Log olarak bilgileri yazdır
+            
             Debug.Log("Spawned Cargo: " + selectedCargo.name + ", Type: " + selectedCargo.cargoType + ", Delivery Deadline: " + selectedCargo.deliveryDeadline + ", Position: " + selectedCargo.locationType);
 
-            // Kargonun gitmesi gereken adresin koordinatlarını al
+            
             if (locationSO.predefinedCoordinates.TryGetValue(selectedCargo.locationType, out Vector3 targetPosition))
             {
                 Debug.Log("Adressss: " + targetPosition);
-                // Ses efektini adresin koordinatlarında yarat
+                
                 GameObject sfxMailBox = Instantiate(sfx, new Vector3(targetPosition.x+13.7f, targetPosition.y+11.5f, targetPosition.z+28.15f), Quaternion.identity);
                 sfxMailBox.name = "SFX for " + selectedCargo.cargoName;
             }
